91d059c78936eabe39e11998397d3ce7afa13ff9
Vishwajit Pawar <pawarvishwajit675@gmail.com>
2026-03-04 19:15:53 +0530

    fix uploads folder creation for render

--- server/server.js
+++ server/server.js
@@ -11,8 +11,14 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: "10mb"}));
-app.use(express.urlencoded({ limit: "10mb", extended: true}));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+const fs = require("fs");
+
+if (!fs.existsSync("uploads")) {
+    fs.mkdirSync("uploads", { recursive: true });
+}
 
 app.use("/uploads", express.static("uploads"));
 
@@ -34,8 +40,8 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI).then(()=> console.log("MongoDB Connected")).catch((err) => console.log(err));
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB Connected")).catch((err) => console.log(err));
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
