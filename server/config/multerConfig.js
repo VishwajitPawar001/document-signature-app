c95482b5f0dc5eac43a1c300660cd36dba88360a
Vishwajit Pawar <pawarvishwajit675@gmail.com>
2026-03-05 22:11:04 +0530

    cloudinary storage

--- server/config/multerConfig.js
+++ server/config/multerConfig.js
@@ -1,24 +1,15 @@
 const multer = require("multer");
-const path = require("path");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("./cloudinary");
 
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, "uploads/");
-    },
-    filename: function(req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname));
-    },
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "documents",
+    resource_type: "raw"
+  }
 });
 
-const upload = multer({
-    storage: storage,
-    fileFilter: function(req, file, cb) {
-        if (file.mimetype === "application/pdf") {
-            cb(null, true);
-        }else {
-            cb(new Error("Only PDFs are allowed"), false);
-        }
-    },
-});
+const upload = multer({ storage });
 
 module.exports = upload;
